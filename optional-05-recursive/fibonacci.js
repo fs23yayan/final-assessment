20fab69c4dbf6fc0793c38fbb41f9212380c7821
Fauzan Suryahadi <55086089+fs23yayan@users.noreply.github.com>
2025-02-28 18:14:52 +0700

    optional recursive belum dites

--- optional-05-recursive/fibonacci.js
+++ optional-05-recursive/fibonacci.js
@@ -1,5 +1,8 @@
 function fibonacci(n) {
-
+    if (n <= 1) {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 // Jangan hapus kode di bawah ini!
---
+++ optional-05-recursive/index.js
@@ -0,0 +1,5 @@
+const factorial = require('./factorial');
+const fibonacci = require('./fibonacci');
+
+console.log('Factorial of 5:', factorial(5)); // Output: 120
+console.log('Fibonacci sequence element at position 7:', fibonacci(7)); // Output: 13
